c32ff07fff5f580163c1d1344f08523c6d3fbdbf
Tydomx <michaelthai16@gmail.com>
2022-07-28 12:40:20 -0500

    Added manager promps and initialized the app

---
+++ index.js
@@ -0,0 +1,105 @@
+// importing dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// employee profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const groupTeamArray = [];
+
+// adding the manager inquirer prompts
+const addManagerPrompts = () => {
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'managerName',
+			message: 'What is your manager\'s name?',
+			validate: managerNameInput => {
+				if (managerNameInput) {
+					return true;
+				} else {
+					console.log('Please enter your manager\'s name!');
+					return false;
+				}
+			}
+		},
+		{
+			type: 'input',
+			name: 'managerId',
+			message: 'What is your manager\'s ID number?',
+			validate: managerIdInput => {
+				if (managerIdInput) {
+					return true;
+				} else {
+					console.log('Pleaes enter the manager\'s ID nubmer');
+					return false;
+				}
+			}
+		},
+		{
+			type: 'input',
+			name: 'managerEmail',
+			message: 'What is the manager\'s Email Address?',
+			validate: managerEmailInput => {
+				if (managerEmailInput) {
+					return true;
+				} else {
+					console.log("Pleaes enter your manager's email address");
+					return false;
+				}
+			}
+		},
+		{
+			type: 'input',
+			name: 'managerOfficeNumber',
+			message: "What is the manager's office number?",
+			validate: managerOfficeNumberInput => {
+				if (managerOfficeNumberInput) {
+					return true;
+				} else {
+					console.log("Please entr the manager's office number");
+					return false;
+				}
+			}
+		}
+	])
+		.then(function (managerData) {
+			const managerName = managerData.managerName;
+			const managerId = managerData.managerId;
+			const managerEmail = managerData.managerEmail;
+			const managerNumber = managerData.managerOfficeNumber;
+			const teamMember = new Manager(managerName, managerId, managerEmail, managerNumber);
+
+			groupTeamArray.push(teamMember);
+
+			addTeamMemberEmployee();
+		})
+};
+
+// function taht initializes the app
+function init() {
+	inquirer.prompt([
+		{
+			message: 'Welcome to the team profile generator. Add your team name',
+			name: 'teamName',
+			validate: teamNameInput => {
+				if (teamNameInput) {
+					return true;
+				} else {
+					console.log("Pleaes add a team name");
+					return false;
+				}
+			}
+		}
+	])
+		.then(function (teamNameData) {
+			const teamName = teamNameData.teamName;
+			groupTeamArray.push(teamName);
+			addManagerPrompts();
+		})
+};
+
+// calling the function taht initializes the app
+init();
