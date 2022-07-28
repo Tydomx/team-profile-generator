3cdf53157077ede4a9c6867602b6ecfac9b75bd8
Tydomx <michaelthai16@gmail.com>
2022-07-28 11:45:27 -0500

    Added employee tests to see if they fail, and readjusted package.json to make sure jest works

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,37 @@
+const Employee = require();
+
+test('Create an employee object', () => {
+	const employee = new Employee('Michael', 123, 'michael@gmail.com');
+
+	expect(employee.name).toBe(expect.any(String));
+	expect(employee.id).toBe(expect.any(Number));
+	expect(employee.email).toBe(expect.any(String));
+});
+
+test('Check to see if can get name with getName()', () => {
+	const name = 'Michael';
+	const employee = new Employee(name, 123, 'michael@gmail.com');
+
+	expect(employee.getName()).toBe(name);
+});
+
+test('Check to see if can get ID with getId()', () => {
+	const id = 123;
+	const employee = new Employee('Michael', id, 'michael@gmail.com');
+
+	expect(employee.getId()).toBe(id);
+});
+
+test('Chekc to see if can get email with getEmail()', () => {
+	const email = 'michael@gmail.com';
+	const employee = new Employee('Michael', 123, email);
+
+	expect(employee.getEmail()).toBe(email);
+});
+
+test('Use getRole() to return "Employee" as the role.', () => {
+	const role = 'Employee';
+	const employee = new Employee('Michael', 123, 'mcihael@gmail.com');
+
+	expect(employee.getRole()).toBe(role);
+});
